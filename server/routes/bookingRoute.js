8e39f03e9bb888f8c820b7f1700b58342d2772c6
Ruthwik Chikoti <chikoti.23bcs10111@ms.sst.scaler.com>
2024-07-13 15:51:52 +0530

    fixed router issue

--- server/routes/bookingRoute.js
+++ server/routes/bookingRoute.js
@@ -1,6 +1,7 @@
-const router = require("express").Router();
+const express = require("express")
+const router = express.Router();
 const stripe = require("stripe")(
-  "sk_test_51JKPQWSJULHQ0FL7LbqLKOaIcjurlUcdP2hJQkXZw3txlhh0hFrEEEOTwdVxf6sWKqLIrerKpV5EfGvmvntYu7Mt00vJq4YQKL"
+  process.env.STRIPE_SECRET_KEY
 );
 const authMiddleware = require("../middlewares/authMiddleware");
 const Booking = require("../models/bookingModel");
